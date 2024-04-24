203fe84a748f5be0018dd846b79a12830a53e739
Mike <mikebio@gmail.com>
2024-04-24 17:27:27 +0000

    Testing php and html

--- index.php
+++ index.php
@@ -1,9 +1,19 @@
+<?php
+
+//Anything here does not get rendered to screen
+$name = "Mike";
+
+?>
+
 <html>
   <head>
     <title>PHP Test</title>
   </head>
   <body>
-    <?php echo '<p>Hello, Class</p>'; ?> 
     <h1> Welcome, everyone</h1>
+
+    <?= "<p>".$name."</p>" ?>
+    
+  </body>
 
 </html>
